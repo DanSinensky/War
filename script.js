3b5f071517368859b082debf7b590598ad8df2c6
Dan Sinensky <dan.i.sinensky@gmail.com>
2023-06-27 11:53:37 -0400

    plays one round

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ class Card {
 }
 
 class Deck {
-  constructor(){
+  constructor(name) {
+    this.name = name
     this.cards = []
+    this.pile = []
     this.createDeck()
   }
 
   createDeck(){
-    const suits = ["Heart", "Spade", "Club", "Diamond"]
+    const suits = ["Hearts", "Spades", "Clubs", "Diamonds"]
     const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 
     for (let i = 0; i < suits.length; i++){
@@ -41,10 +43,38 @@ class Deck {
   }
 }
 
-let firstDeck = new Deck()
-let secondDeck = new Deck()
+class Game {
+  constructor() {
+    this.winner = undefined
+    this.loser = undefined
+    this.battle = []
+  }
+
+  match() {
+    let winner
+    let loser
+    let tie = false
+    firstDeck.cards[0].score > secondDeck.cards[0].score ? [this.winner = firstDeck, this.loser = secondDeck] :
+    firstDeck.cards[0].score < secondDeck.cards[0].score ? [this.winner = secondDeck, this.loser = firstDeck] : 
+    tie = true
+    if (tie = true) {
+   //   this.war()
+    }
+    console.log(`${this.winner.name} won by playing the ${this.winner.cards[0].rank} of ${this.winner.cards[0].suit}
+    against ${this.loser.name}'s ${this.loser.cards[0].rank} of ${this.loser.cards[0].suit}. Player One has ${firstDeck.cards.length} 
+    cards in their deck and Player Two has ${secondDeck.cards.length} cards in their deck.`)
+    this.battle.push(this.winner.cards.shift(), this.loser.cards.shift())
+    this.winner.pile = this.winner.pile.concat(this.battle)
+    console.log(firstDeck.pile, secondDeck.pile)
+  }
+}
+
+const game = new Game()
+const firstDeck = new Deck("Player One")
+const secondDeck = new Deck("Player Two")
 secondDeck.cards = []
 for (let i = 0; i < 26; i++){
   secondDeck.cards.push(firstDeck.cards.pop())
 }
 console.log(firstDeck, secondDeck)
+game.match()

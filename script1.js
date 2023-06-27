78f976ff14b423766513da71459ee07289bf4ec6
Dan Sinensky <dan.i.sinensky@gmail.com>
2023-06-27 17:01:46 -0400

    raul's version

--- script1.js
+++ script1.js
@@ -56,6 +56,46 @@ class Game {
     this.p1.push(...cards.splice(0, 26))
     this.p2.push(...cards)
   }
+
+  playGame() {
+    while (this.p1.length > 0 && this.p2.length > 0) {
+      let p1Card = this.p1.pop()
+      let p2Card = this.p2.pop()
+
+      if (p1Card.score === p2Card.score) {
+        console.log("War!")
+        this.pile.push(p1Card, p2Card)
+        this.war()
+      } else if (p1Card.score > p2Card.score) {
+        this.p1.unshift(p2Card, p1Card, ...this.pile.splice(0))
+        console.log("Player One wins the round!")
+      } else {
+        this.p2.unshift(p1Card, p2Card, ...this.pile.splice(0))
+        console.log("Player Two wins the round")
+      }
+    }
+
+    if (this.p1.length > 0) {
+      console.log(`Player 1 wins with ${this.p1.length} cards`)
+    } else {
+      console.log(`Player 2 wins with ${this.p2.length} cards`)
+    }
+  }
+
+  war() {
+    if (this.p1.length < 4 || this.p2.length < 4) {
+      if (this.p1.length < 4) {
+        this.p2.push(...this.p1.splice(0),...this.pile.splice(0))
+      } else {
+        this.p1.push(...this.p2.splice(0),...this.pile.splice(0))
+      }
+    } else {
+      let p1WarPile = this.p1.splice(-3, 3)
+      let p2WarPile = this.p2.splice(-3, 3)
+      this.pile.push(...p1WarPile,...p2WarPile)
+    }
+  }
 }
 
 const game = new Game()
+game.playGame()
